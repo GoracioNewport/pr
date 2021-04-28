26b86748c87962669903ee663bf45f132df6a567
GoracioNewport <bdfyljkub@gmail.com>
2021-04-28 16:55:01 +0600

    Main PC

--- study/dp/informatics/5D.cpp
+++ study/dp/informatics/5D.cpp
@@ -36,53 +36,100 @@ double eps = 1e-12;
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((ll)(x).size())
 
+struct b {
+  ll len;
+  bool possible;
+  string p;
+  bool operator< (b a) {
+    return len < a.len;
+  }
+};
+
 int main() {
   fast_cin();
 
   string s1, s2;
   cin >> s1 >> s2;
   ll n = sz(s1), m = sz(s2);
-  vector <vector <string>> dp(n + 1, vector <string> (m + 1, ""));
-  dp[0][0] = "";
-  if (s1[0] == '*') dp[1][0] = "";
-  if (s2[0] == '*') dp[0][1] = "";
-  forsn(i,2,n + 1) {
-    if (dp[i - 1][0] == "" && s1[i - 1] == '*') dp[i][0] = "";
-  } forsn(i,2,m + 1) {
-    if (dp[0][i - 1] == "" && s2[i - 1] == '*') dp[0][i] = "";
-  }
-
+  vector <vector <b>> dp(n + 1, vector <b> (m + 1, {0, false, ""}));
+  dp[0][0].possible = true;
+  
   forsn(i,1,n + 1) {
     forsn(j,1,m + 1) {
-      if (s1[i - 1] == s2[j - 1]) {
-        if (s1[i - 1] == '?') dp[i][j] = dp[i - 1][j - 1] + 'a';
-        else if (s1[i - 1] == '*') dp[i][j] = dp[i - 1][j - 1];
-        else dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
-      } else {
-        if (s1[i - 1] == '?' && s2[j - 1] == '*') dp[i][j] = dp[i - 1][j] + 'a';
-        else if (s1[i - 1] == '*' && s2[j - 1] == '?') dp[i][j] = dp[i][j - 1] + 'a';
-        else if (s1[i - 1] == '?' && isalpha(s2[j - 1])) dp[i][j] = dp[i - 1][j - 1] + s2[j - 1];
-        else if (isalpha(s1[i - 1]) && s2[j - 1] == '?') dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
-        else if (s1[i - 1] == '*' && isalpha(s2[j - 1])) dp[i][j] = dp[i - 1][j];
-        else if (isalpha(s1[i - 1]) && s2[j - 1] == '*') dp[i][j] = dp[i][j - 1];
-        else dp[i][j] = '#';
+      vector <b> pos;
+      if (s1[i - 1] == s2[j - 1] && isalpha(s1[i - 1])) {
+        b x = dp[i - 1][j - 1];
+        x.len++;
+        x.p += s1[i - 1];
+        pos.pb(x);
+      } else if (s1[i - 1] == '?' && s2[j - 1] == '?') {
+        b x = dp[i - 1][j - 1];
+        x.len++;
+        x.p += 'a';
+        pos.pb(x);
+      } else if (s1[i - 1] == '*' && s2[j - 1] == '*') pos.pb(dp[i - 1][j - 1]);
+      else if (s1[i - 1] == '?' && isalpha(s2[j - 1])) {
+        b x = dp[i - 1][j - 1];
+        x.len++;
+        x.p += s2[j - 1];
+        pos.pb(x);
+      } else if (isalpha(s1[i - 1]) && s2[j - 1] == '?') {
+        b x = dp[i - 1][j - 1];
+        x.len++;
+        x.p += s1[i - 1];
+        pos.pb(x);
+      } else if (s1[i - 1] == '*' && isalpha(s2[j - 1])) {
+         b x = dp[i - 1][j - 1];
+         b y = dp[i - 1][j];
+         b z = dp[i][j - 1];
+         x.len++;
+         z.len++;
+         x.p += s2[j - 1];
+         z.p += s2[j - 1];
+         pos.pb(x);
+         pos.pb(y);
+         pos.pb(z);
+      } else if (isalpha(s1[i - 1]) && s2[j - 1] == '*') {
+        b x = dp[i - 1][j - 1];
+        b y = dp[i - 1][j];
+        b z = dp[i][j - 1];
+        x.len++;
+        y.len++;
+        x.p += s1[i - 1];
+        y.p += s1[i - 1];
+        pos.pb(x);
+        pos.pb(y);
+        pos.pb(z);
+      } else if (s1[i - 1] == '*' && s2[j - 1] == '?') {
+        b x = dp[i - 1][j - 1];
+        b y = dp[i - 1][j];
+        b z = dp[i][j - 1];
+        x.len++;
+        z.len++;
+        x.p += 'a';
+        z.p += 'a';
+        pos.pb(x);
+        pos.pb(y);
+        pos.pb(z);
+      } else if (s1[i - 1] == '?' && s2[j - 1] == '*') {
+        b x = dp[i - 1][j - 1];
+        b y = dp[i - 1][j];
+        b z = dp[i][j - 1];
+        x.len++;
+        y.len++;
+        x.p += 'a';
+        y.p += 'a';
+        pos.pb(x);
+        pos.pb(y);
+        pos.pb(z);
+      } vector <b> posFin;
+      for (auto &i : pos) {
+        if (i.possible) posFin.pb(i);
+      } sort(all(posFin));
+      if (sz(posFin)) {
+        dp[i][j] = posFin[0];
       }
     }
-  } if (dp[n][m][0] == '#') cout << "No solution!" << ln;
-  else cout << dp[n][m] << ln;
-
-  ll ind = -1;
-  cout << "     .  ";
-  for (auto &i : s2) cout << i << "   ";
-  cout << ln;
-  for (auto &i : dp) {
-    if (ind == -1) {
-      cout << ".";
-      ind++;
-    } else cout << s1[ind++];
-    cout << " - ";
-    for (auto &j : i) cout << "<" << j << "> ";
-    cout << ln;
-  }
+  } cout << (dp[n][m].possible ? dp[n][m].p : "No solution!") << ln;
 
 }
